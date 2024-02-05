c0d57f32980998f18529f620e1a4542f298d4dd2
Crowdbotics <team@crowdbotics.com>
2024-02-05 18:21:36 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "smartbuildingenergy-45962",
+  url: "https://smartbuildingenergy-45962.botics.co",
+  api: "https://smartbuildingenergy-45962.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
